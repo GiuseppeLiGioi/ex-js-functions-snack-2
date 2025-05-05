5daff9d49204a793294658ae52700a09e4eabbb2
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-05-05 12:37:03 +0200

    added: html file to display the console in the browser.

---
+++ script.js
@@ -0,0 +1,15 @@
+/* 🏆 Snack 1
+
+Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile
+Quindi, riscrivi la funzione somma con la sintassi delle arrow functions. */
+
+
+// Crea una funzione che somma due numeri.
+
+
+//Crea una funzione dichiarativa chiamata somma che accetta due numeri e restituisce la loro somma.
+function somma (n1, n2){
+    return console.log(n1+n2);
+}
+
+somma();
