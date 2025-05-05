9787574ab4e250afde22cd976e63fac22eb14403
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-05-05 15:46:11 +0200

    added. snack 7 and test it.

--- script.js
+++ script.js
@@ -120,3 +120,30 @@ return function avviaContatore(){
 
 const start = creaContatoreAutomatico(1000);
 start();
+
+
+/*
+🏆 Snack 7
+Crea una funzione che ferma un timer dopo un certo tempo
+Scrivi una funzione eseguiEferma che accetta un messaggio, un tempo di avvio e un tempo di stop. 
+Il messaggio deve essere stampato a intervalli regolari, ma si deve fermare dopo il tempo di stop.
+*/
+
+
+function eseguiEferma (messaggio, tavv, tstop){
+setTimeout(() => {
+console.log(messaggio)
+
+const intervallo = setInterval(() => {
+    console.log(messaggio)
+}, 1000)
+
+setTimeout(() => {
+    clearInterval(intervallo);
+    console.log("Tempo scaduto!")
+},tstop);
+
+}, tavv);
+}
+
+eseguiEferma("Ciao Giuse", 3000, 10000);
