d9e3d941b357bfc6a518ca634d31e4f51a06f036
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-05-05 13:06:35 +0200

    add: snack 2 and test it.

--- script.js
+++ script.js
@@ -25,3 +25,13 @@ const arrowSomma = (n1,n2) => {
 }
 
 arrowSomma(20, 20) //output 40
+
+
+//🏆 Snack 2
+//Crea una arrow function che calcola il quadrato di un numero.
+
+//Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.
+
+const quadrato = numero => console.log(numero * numero);
+
+quadrato(10); //output 100
