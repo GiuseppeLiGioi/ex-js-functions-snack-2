3acc4b1679aa626a165a7ac88294abb6b79c5325
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-05-05 12:50:49 +0200

    added: snack 1.

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-/* 🏆 Snack 1
-
-Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile
-Quindi, riscrivi la funzione somma con la sintassi delle arrow functions. */
-
+// 🏆 Snack 1
 
 // Crea una funzione che somma due numeri.
-
 
 //Crea una funzione dichiarativa chiamata somma che accetta due numeri e restituisce la loro somma.
 function somma (n1, n2){
     return console.log(n1+n2);
 }
 
-somma();
+somma(10,5); //output 15
+
+//Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile
+
+const sommaAnonima = function (n1, n2){
+    return console.log(n1 + n2);
+}
+
+sommaAnonima(20, 10); //output 30
+
+
+
+//Quindi, riscrivi la funzione somma con la sintassi delle arrow functions. 
+const arrowSomma = (n1,n2) => {
+    return console.log(n1 + n2);
+}
+
+arrowSomma(20, 20) //output 40
